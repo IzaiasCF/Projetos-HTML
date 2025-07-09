63378d9cca42b9068ddb0b96a8631a53590a018b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-09 10:14:38 -0300

    add commit

---
+++ push_projetos_html.bat
@@ -0,0 +1,11 @@
+@echo off
+:: Caminho até sua pasta local do repositório
+cd /d "C:\Users\IzaiasCF\Documents\Projetos\Projetos-HTML"
+:: Mensagem de commit padrão (você pode editar)
+set /p msg="Digite a mensagem do commit: "
+:: Executa os comandos Git
+git add .
+git commit -m "%msg%"
+git push origin main
+
+pause
